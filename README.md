cc5f38729e77655e539d6261e0fb2fb9d0fd8370
John Fellman <john.fellman@avrs.com>
2015-03-20 09:28:25 -0700

    fixes instructions

--- README.md
+++ README.md
@@ -5,11 +5,9 @@ This is a little unusual, so I have taken care of this for you.
 I have also provided the router (and names for your collection and model classes).
 
 ##Instructions:
-1. Write a collection that gets its data from 'http://www.jengascript.org/data/'
+1. Write a collection that gets its data from 'http://www.jengascript.com/data/'
 2. Write a view (and Template) that displays basic info from all the models in this collection with links to an individual view
 3. Write this individual view that displays a form for updating an individual model
-3. Make that form update the correct property on the correct model when an input in the form is changed.
-4. Add a save button that saves the correct individual model (with the properties properly changes per step 2).
-5. This save will fail every time. Handle the error with a message - you MAY NOT use alert().
-6. Make this error message dismissable in some way.
-7. BONUS: Add some CSS to make it look not horrible.
+4. Make that form update the correct property on the correct model when an input in the form is changed.
+5. Add a save button that saves the correct individual model (with the properties properly changed per step 2).
+6. BONUS: Add some CSS to make it look not horrible.
--- main.js
+++ main.js
@@ -64,14 +64,12 @@ window.addEventListener('load', function () {
     // home view - views are defined after window has loaded because the bootstrapped templates exist at this time
     app.views.home = Backbone.View.extend({
         instructions:  [
-            "Write a collection that gets its data from 'http://www.jengascript.org/data/' </li>",
-            "Write a view (and Template) that displays basic info from all the models in this collection with links to an individual view </li>",
-            "Write this individual view that displays a form for updating an individual model </li>",
-            "Make that form update the correct property on the correct model when an input in the form is changed. </li>",
-            "Add a save button that saves the correct individual model (with the properties properly changes per step 2). </li>",
-            "This save will fail every time. Handle the error with a message - you MAY NOT use alert(). </li>",
-            "Make this error message dismissable in some way. </li>",
-            "BONUS: Add some CSS to make it look not horrible.</li>", 
+            "Write a collection that gets its data from 'http://www.jengascript.com/data/'",
+            "Write a view (and Template) that displays basic info from all the models in this collection with links to an individual view",
+            "Write this individual view that displays a form for updating an individual model",
+            "Make that form update the correct property on the correct model when an input in the form is changed.",
+            "Add a save button that saves the correct individual model (with the properties properly changes per step 2).",
+            "BONUS: Add some CSS to make it look not horrible."
         ],
         render: function () {
             this.$el.html(_.template(this.template)({ instructions: this.instructions }));
